e2acd140e42a079e60a9126faa45424b18c9590b
robemike <robemike1135@gmail.com>
2024-05-28 08:19:32 +0300

    completes tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word.lower())
+    
+    
+    def match(self, anagrams):
+        sorted_anagrams = [sorted(possible_anagram.lower()) for possible_anagram in anagrams]
+        return [anagram for anagram in anagrams if sorted_anagrams[anagrams.index(anagram)] == self.word]
